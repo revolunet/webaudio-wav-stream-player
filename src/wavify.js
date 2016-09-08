41e954087c2224005dbc58bcebef9c006c0d8df6
chrisguttandin <chrisguttandin@media-codings.com>
2016-09-08 02:32:26 +0200

    fix wav construction

--- src/wavify.js
+++ src/wavify.js
@@ -24,11 +24,11 @@ const wavify = (data, numberOfChannels, sampleRate) => {
 
     d.setUint32(16, 16, true);
     d.setUint16(20, 1, true);
-    d.setUint16(22, 1, true);
     d.setUint16(22, numberOfChannels, true);
     d.setUint32(24, sampleRate, true);
     d.setUint32(28, sampleRate * 1 * 2);
     d.setUint16(32, numberOfChannels * 2);
+    d.setUint16(34, 16, true);
 
     d.setUint8(36, 'd'.charCodeAt(0));
     d.setUint8(37, 'a'.charCodeAt(0));
